841550e65a916c6f1f3405af807bd43e54bb51b1
Mark Kowalski <mark.kowalski86@gmail.com>
2019-07-02 14:49:25 +0200

    Sealed HttpResponse class

--- dry/src/main/java/dev/mko/dry/http/HttpResponse.kt
+++ dry/src/main/java/dev/mko/dry/http/HttpResponse.kt
@@ -1,2 +1,6 @@
 package dev.mko.dry.http
 
+sealed class HttpResponse<T> {
+    data class Success<T>(val data: T) : HttpResponse<T>()
+    data class Failure<T>(val error: Throwable) : HttpResponse<T>()
+}
